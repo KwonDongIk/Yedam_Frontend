7c0f7d7168db21c3733b05d7ea29f644fd42c070
KwonDongIk <dongikik@kakao.com>
2025-01-17 17:48:02 +0900

    0117 PM 17:47 commit

--- 0117/javascript/object_4.js
+++ 0117/javascript/object_4.js
@@ -1,13 +1,19 @@
 // object_4.js
+// 객체는 주소값을 비교
 
-let month = 2;
-let date = 0;
-let year = 2024;
+
+
+let month = 4;
+let date = 17;
+let year = 2022;
 
 let today = new Date();
+let now = new Date();
+console.log(today == now);
+
 
 today.setFullYear(year);
 today.setMonth(month); // ※ 1월 => 0부터
 today.setDate(date);
 
-console.log(today, today.getDate());
+console.log(today);
